b7f4242d176b1f58f77161fc48d1c1bc78e25623
Mnishek <shavyrina.marina@mail.ru>
2021-07-28 14:47:36 +0300

    homework_4

---
+++ dz_3/src/components/Input.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+import { makeStyles } from '@material-ui/core/styles';
+
+
+
+const useStyles = makeStyles({
+    buttonSend: {
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        border: 0,
+        borderRadius: 7,
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+        color: 'white',
+        height: 48,
+        padding: '0 30px',
+        marginLeft: '30px',
+    },
+});
+
+
+
+
+
+const Input = (props) => {
+    const { onSubmit } = props
+
+    const classes = useStyles();
+
+    const [inputValue, setInputValue] = useState('')
+
+    const handleChange = (e) => {
+        setInputValue(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        if (onSubmit) {
+            onSubmit(inputValue)
+            setInputValue('')
+        }
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <TextField
+                placeholder="Enter your message"
+                required
+                autoFocus
+                id="outlined-required"
+                label="Message"
+                variant="outlined"
+                value={inputValue}
+                onChange={handleChange}
+            />
+
+            <Button type="submit" className={classes.buttonSend}>SEND</Button>
+        </form>
+    )
+}
+
+export default Input
