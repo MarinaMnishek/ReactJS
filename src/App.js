4175d3566d7cccb3fe33b3eab9e6eab8c0342103
Mnishek <shavyrina.marina@mail.ru>
2021-08-04 10:50:43 +0300

    homework_7

--- src/App.js
+++ src/App.js
@@ -10,34 +10,6 @@ function App() {
   // const  chatList  = useSelector((state) => state.chatList)
 
 
-  // const [chats, setChats] = useState([
-  //   { id: 'chat1', name: 'Чат 1' },
-  //   { id: 'chat2', name: 'Чат 2' },
-  //   { id: 'chat3', name: 'Чат 3' },
-  // ])
-  // const [currentChat, setCurrentChat] = useState(chatList[0])
-
-  // const handleChangeChat = (chat) => setCurrentChat(chat)
-
-  // const handleAddChat = (chatName) => {
-  //   setChats((currentChats) => [
-  //     ...currentChats,
-  //     { name: chatName, id: `chat${Date.now()}` },
-  //   ])
-  // }
-
-  // const handleRemoveChat = (chatId) => {
-  //   setChats((currentChats) =>
-  //     currentChats.filter((chat) => chat.id !== chatId)
-  //   )
-  // }
-
-  // const handleIsChatExists = React.useCallback(
-  //   (chatId) => {
-  //     return Boolean(chats.find((chat) => chat.id === chatId))
-  //   },
-  //   [chats]
-  // )
 
 
   return (
@@ -50,12 +22,7 @@ function App() {
       </div>
 
       <Router
-        // chats={chats}
-        // currentChat={currentChat}
-        // onCurrentChatChange={handleChangeChat}
-        // getIsChatExists={handleIsChatExists}
-        // onAddChat={handleAddChat}
-        // onRemoveChat={handleRemoveChat}
+        
       />
 
     </div >
--- src/index.js
+++ src/index.js
@@ -5,15 +5,18 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App'
 import { Provider } from 'react-redux';
-import { store } from './store/store'
+import { persistor, store } from './store/store'
+import { PersistGate } from 'redux-persist/integration/react'
 
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
